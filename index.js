09cb2d2d3173ade84cd9d43e2a89c4068eb0a6d3
joegelay <joegelay@gmail.com>
2020-04-02 10:29:47 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+
+const destinationURL = "http://localhost:3000/users"
+const formData = {
+    name: userName,
+    email: userEmail
+}
+
+const configurationObject = {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+};
+
+return fetch(destinationURL, configurationObject)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object){
+        document.body.innerHTML = object["id"]
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    });
+}
